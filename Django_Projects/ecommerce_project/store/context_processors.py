d28c68561a84b753bcc178db65a7164d232870a7
Md. Ridwan Mizan <ridwanmizan@gmail.com>
2020-03-27 10:28:03 +0600

    first commit

---
+++ Django_Projects/ecommerce_project/store/context_processors.py
@@ -0,0 +1,26 @@
+from .models import Category, Cart, CartItem
+from .views import _cart_id
+
+
+def menu_links(request):
+    links = Category.objects.all()
+    # print(f"links is: {links} and dict is {dict(links=links)}")
+    return dict(links=links)
+
+
+def counter(request):
+    item_count = 0
+    if 'admin' in request.path:
+        return {}
+    else:
+        try:
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            for cart_item in cart_items:
+                item_count += cart_item.quantity
+        except Cart.DoesNotExist:
+            item_count = 0
+    return dict(item_count=item_count)
+
+
+
